bce5acd04a22b47a76ef7deb027677d88e238775
Pranav Pore <pranav@pranavpore.com>
2023-07-15 11:32:54 +0530

    completed task-manager

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-const connectDB = require('./db/connect');
-require('dotenv').config();
-const port = 3000;
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+const port = process.env.PORT || 3000;
 
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandler);
 
-const start = async() => {
+const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`Server is listening on 127.0.0.1:${port}`));
-    } catch(error) {
+        app.listen(
+            port,
+            console.log(`Server is listening on 127.0.0.1:${port}`)
+        );
+    } catch (error) {
         console.log(error);
     }
-}
+};
 
 start();
