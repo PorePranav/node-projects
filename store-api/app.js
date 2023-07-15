0c8ee26a303427f1758d31101f4942b2b5138c11
Pranav Pore <pranav@pranavpore.com>
2023-07-15 23:57:34 +0530

    set up routes

---
+++ store-api/app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const router = require("./routes/products");
+//async errors
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+    res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
+});
+
+app.use("/api/v1/products", router);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+//products route
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server started on 127.0.0.1:${port}`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
